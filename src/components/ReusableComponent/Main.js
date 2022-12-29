2b3ef7df28c697a887c7efdc7ac18ca059a52052
Manu <manushaji717@gmail.com>
2022-12-29 18:40:54 +0530

    Begining to code

---
+++ src/components/ReusableComponent/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// try writing all the code here..
+
+function Main() {
+  return (
+    <div>
+      <h1>the reualrkgcnhwth</h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/ReusableComponent/Main'
+import Main from '../components/ReusableComponent/Main';
+
+function Home() {
+  return (
+    <>
+        <Main />
+    </>
+  )
+}
+
+export default Home
